35d8b77f2b8dca85280ccd70305899b213a3efed
Tyler Tackett <ttackett412@yahoo.com>
2017-10-09 13:51:08 -0400

    Committing the final project

--- main.cpp
+++ main.cpp
@@ -1,25 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: ttackett
- *
- * Created on September 25, 2017, 2:41 PM
- */
-
-#include <cstdlib>
+#include <iostream> 
 
 using namespace std;
+class FarmAnimal{ 
+public:
+    FarmAnimal(double water_consumption); 
+    double getWaterConsumption();
+// ...
+private:
+    double water_consumption;
+};
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+FarmAnimal::FarmAnimal(double water_consumption) { 
+    this->water_consumption = water_consumption;}
 
-    return 0;
+double FarmAnimal::getWaterConsumption() { 
+    return water_consumption;}
+
+class DummyAnimal : public FarmAnimal{ 
+public:
+    DummyAnimal(); 
+};
+
+DummyAnimal::DummyAnimal() : FarmAnimal(10.0) 
+{
+    
 }
+
+class DoublingAnimal : public FarmAnimal {
+public:
+    DoublingAnimal(double given_water_consumption); 
+};
+
+DoublingAnimal::DoublingAnimal(double given_water_consumption) 
+: FarmAnimal(2 * given_water_consumption) {
+    
+}
+
+void printConsumption(FarmAnimal animal) {
+    cout << "This animal consumes " << animal.getWaterConsumption() << " liters of water" << endl;
+}
+
+int main() 
+{
+    FarmAnimal animalA(5); 
+    DummyAnimal animalB; 
+    DoublingAnimal animalC(21);
+
+cout << "animalA consumes " << animalA.getWaterConsumption() 
+        << " liters of water." << endl;
+cout << "What about the others?" << endl; 
+
+printConsumption(animalB);
+printConsumption(animalC);
+
+return 0; }
 
